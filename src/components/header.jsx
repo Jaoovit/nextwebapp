a1763385530a16dec15c702e1b364eccd534e59b
Joao Oliveira <jaoovit@gmail.com>
2023-05-11 10:07:44 +0100

    create a header style

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,10 +1,14 @@
 import Link from "next/link"
+import styles from "../styles/header.module.css"
 
 export default function Header(props) {
     return(
         <div>
-            <h1>{props.title}</h1>
+            <div className={styles.backlink}>
             <Link href="/">Back</Link>
+            {props.children}
+            </div>
+            <h1>{props.title}</h1>
         </div>
     )
 }
