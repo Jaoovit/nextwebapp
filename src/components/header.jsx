7817ba26304e9b31bf280800514aede74e9ce9a5
Joao Oliveira <jaoovit@gmail.com>
2023-05-12 11:56:22 +0100

    modify the backbutton style

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -3,11 +3,11 @@ import styles from "../styles/header.module.css"
 
 export default function Header(props) {
     return(
-        <div>
-            <div className={styles.backlink}>
-            <Link href="/">Back</Link>
-            {props.children}
+        <div className={styles.layout}>
+            <div className={styles.backbutton}>
+                <Link href="/">Back</Link>
             </div>
+                {props.children}
             <h1>{props.title}</h1>
         </div>
     )
