f1b4b9e2acbf29b0aa464c853d87f25edf80ade3
Joao Oliveira <jaoovit@gmail.com>
2023-05-17 10:11:08 +0100

    create a increase onClick button

--- src/pages/products.jsx
+++ src/pages/products.jsx
@@ -1,11 +1,28 @@
-import Header from "../components/header"
-import Footer from "../components/footer"
+import { useState } from "react";
+import Header from "../components/header";
+import Footer from "../components/footer";
 
 export default function products() {
+  const state = useState(0)
+
+  let number = state[0]
+  let alterNumber = state[1]
+
+  console.log(state)
+  
+  function increase() {
+    alterNumber(number + 1)
+  }
+
     return(
         <div>
-            <Header title= "Products" />
+            <Header title= "Products"> 
+            <div>{number}</div>
+            <button onClick={increase}>increase</button>            
+            </Header>
             <Footer />
         </div>
+             
+        
     )
 }
