b6b7938a843964246decc95d1572573ec005746d
Joao Oliveira <jaoovit@gmail.com>
2023-05-17 10:54:34 +0100

    add a useRouter

--- src/pages/clients/[code].jsx
+++ src/pages/clients/[code].jsx
@@ -1,9 +1,12 @@
 import Header from "../../components/header";
+import { useRouter } from 'next/router'
 
 export default function ClientCode () {
+    const router = useRouter()
+
     return (
         <Header title="Dynamic routes">
-            <span>Code = ????</span>
+            <span>Code = {router.query.code}</span>
         </Header>
     )
 }
